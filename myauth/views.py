fb42aa8b76be7badc0ae103788994a0707fe625f
burkouski <burkouski.vs@gmail.com>
2015-01-16 17:50:24 +0200

    add registration

--- myauth/views.py
+++ myauth/views.py
@@ -1,26 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, RequestContext
 from django.contrib.auth.models import User
-from myauth.forms import UserForm
+from myauth.forms import UserForm, DivErrorList
+from django.core.exceptions import ValidationError
+
 
 def register(request):
     registered = False
-
+    mess = ''
     if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
+        user_form = UserForm(request.POST, error_class=DivErrorList)
 
         if user_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
+            username = user_form.cleaned_data['username']
+            password = user_form.cleaned_data['password']
+            email = user_form.cleaned_data['email']
+            user = User.objects.create_user(username, email, password)
+            user.is_active = True
             user.save()
-
             registered = True
+            user_form = UserForm()
+            mess = u'Регистрация прошла успешно! '
 
     # Not a HTTP POST, so we render our form using two ModelForm instances.
     # These forms will be blank, ready for user input.
     else:
-        user_form = UserForm()
+        user_form = UserForm(error_class=DivErrorList)
 
     # Render the template depending on the context.
     return render(request,
-            'auth/register.html',
-            {'user_form': user_form, 'registered': registered} )
+            'myauth/register.html', {'user_form': user_form, 'mess': mess}, context_instance=RequestContext(request) )
