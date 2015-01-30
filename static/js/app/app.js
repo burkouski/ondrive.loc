ab5431b56e9b84f52da6912827bff2f03221dc43
burkouski <burkouski.vs@gmail.com>
2015-01-30 17:11:55 +0200

    add acordion

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -1,4 +1,8 @@
-ymapsApp =angular.module('ymapsApp', ['ymaps', 'ngResource', 'caco.ClientPaginate', 'ngCookies', 'checklist-model']).run(function($http, $cookies) {
-    $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
-    $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-});
+ymapsApp = angular.module('ymapsApp', ['ymaps', 'ngResource', 'caco.ClientPaginate', 'ngCookies', 'checklist-model'])
+    .config(function ($interpolateProvider) {
+        $interpolateProvider.startSymbol('{$');
+        $interpolateProvider.endSymbol('$}');
+    }).run(function ($http, $cookies) {
+        $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
+        $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+    });
