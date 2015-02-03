e7aab54d64c190c9cea72e7e6a4c768b00aca347
burkouski <burkouski.vs@gmail.com>
2015-02-03 17:58:26 +0200

    add map and worl list in detail_view.html

--- service/views.py
+++ service/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import get_object_or_404, get_list_or_404, render, HttpResponse
 from service.models import AutoService, ElectricianWork
 import json
+from django.views.decorators.csrf import ensure_csrf_cookie
+
 
 def autoservice_detail(request, service_alias):
     service = get_object_or_404(AutoService, alias=service_alias)
+    rel = AutoService._meta.get_m2m_with_model()
 
-    args = {'service': service}
+    args = {'service': service, 'rel': rel}
     return render(request, 'service/detail_view.html', args)
 
-
+@ensure_csrf_cookie
 def autoservice_list(request):
     services = AutoService.objects.all()
     obj = {}
@@ -18,7 +21,6 @@ def autoservice_list(request):
     for key, val in obj.items():
         for x in val:
             services = services.filter(**{ key: x })
-
 
     services = json.dumps([{
         #'dls': obj,
@@ -30,7 +32,6 @@ def autoservice_list(request):
                             'address': o.address,
                             'url': o.get_absolute_url()} for o in services])
     if request.method == 'POST':
-
         return HttpResponse(services)
 
     return render(request, 'service/list_view.html')
--- static/js/app/app.js
+++ static/js/app/app.js
@@ -3,6 +3,8 @@ ymapsApp = angular.module('ymapsApp', ['ymaps', 'ngResource', 'caco.ClientPagina
         $interpolateProvider.startSymbol('{$');
         $interpolateProvider.endSymbol('$}');
     }).run(function ($http, $cookies) {
+
         $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
         $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+        console.log( $http.defaults.headers.post['X-CSRFToken']);
     });
