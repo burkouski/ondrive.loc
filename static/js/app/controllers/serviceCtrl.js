adc7ffe9e7ed68040b30b639579b698cdb087843
burkouski <burkouski.vs@gmail.com>
2015-02-23 11:52:12 +0200

    fix

--- static/js/app/controllers/serviceCtrl.js
+++ static/js/app/controllers/serviceCtrl.js
@@ -1,5 +1,34 @@
 ymapsApp.controller('serviceCtrl', ['$scope', function ($scope) {
 
 
+$scope.starsInit = function(rating) {
+    var rating = parseFloat(rating.replace(",", ".")),
+        positiveStar = ~~rating,
+        halfStar = 0,
+        negativeStar,
+        fractional = rating - positiveStar;
+console.log(rating,positiveStar,fractional)
+    if (fractional > 0.25 && fractional < 0.85 ) {
+        halfStar = 1;
+    }
+
+    else if (fractional > 0.85) {
+        positiveStar += 1;
+    }
+    else {
+        halfStar = 0;
+    }
+    negativeStar = 5 - positiveStar - halfStar;
+    console.log(halfStar)
+    return {
+        positiveStar: _getArray(positiveStar),
+        halfStar: _getArray(halfStar),
+        negativeStar: _getArray(negativeStar)
+    }
+};
+
+_getArray=function(n){
+     return new Array(n);
+};
 
 }]);
